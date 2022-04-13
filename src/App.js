895146040a34a5c4d2432235fc78876d2d4a8739
mitchellnel <mitchellmclin@gmail.com>
2022-04-13 15:54:04 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,67 @@
-import twitterLogo from './assets/twitter-logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import twitterLogo from "./assets/twitter-logo.svg";
+import "./App.css";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = "nellari21";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
-          <p className="sub-text">
-            View your GIF collection in the metaverse ✨
-          </p>
+    // work out if a Phantom wallet has been found
+    const checkIfWalletIsConnected = async () => {
+        try {
+            const { solana } = window;
+
+            if (solana) {
+                if (solana.isPhantom) {
+                    console.log("Phantom wallet found!");
+                }
+            } else {
+                console.log(
+                    "Object solana not found... Perhaps you don't have a Phantom wallet?"
+                );
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    // when App mounts, check if we have a Phantom wallet connected
+    useEffect(() => {
+        const onLoad = async () => {
+            await checkIfWalletIsConnected();
+        };
+
+        window.addEventListener("load", onLoad);
+
+        return () => window.removeEventListener("load", onLoad);
+    }, []);
+
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="header-container">
+                    <p className="header">🏎 F1 GIF Portal</p>
+                    <p className="sub-text">
+                        F1 GIFs living on the Solana blockchain 🏁
+                    </p>
+                </div>
+                <div className="footer-container">
+                    <img
+                        alt="Twitter Logo"
+                        className="twitter-logo"
+                        src={twitterLogo}
+                    />
+                    <a
+                        className="footer-text"
+                        href={TWITTER_LINK}
+                        target="_blank"
+                        rel="noreferrer"
+                    >{`built on @${TWITTER_HANDLE}`}</a>
+                </div>
+            </div>
         </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default App;
